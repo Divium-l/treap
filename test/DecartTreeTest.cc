af513b98b5fd53e2bc5eecb945373f43ba00fd70
Egor-Always-AFK <52970978+Egor-Always-AFK@users.noreply.github.com>
2023-12-09 22:30:55 +0300

    Some tests

--- test/DecartTreeTest.cc
+++ test/DecartTreeTest.cc
@@ -1,3 +1,37 @@
 //
 // Created by Egor-Always-AFK on 07.12.2023.
 //
+
+#include <gtest/gtest.h>
+#include "../src/DecartTree.hpp"
+
+TEST(DecartTreeTest, CreateTreeSuccess) {
+    auto *tree = new mit::DecartTree<int>();
+
+    auto root = tree->getRoot();
+    ASSERT_EQ(root, nullptr);
+}
+
+
+TEST(DecartTreeTest, InsertIntoTreeSuccess) {
+    auto *tree = new mit::DecartTree<int>();
+
+    tree->add(10, 100);
+    tree->add(30, 120);
+    tree->add(20, 10);
+
+    auto root = tree->getRoot();
+
+    ASSERT_EQ(root->value, 10);
+    ASSERT_EQ(root->priority, 100);
+    ASSERT_EQ(root->left->value, 20);
+    ASSERT_EQ(root->left->priority, 10);
+    ASSERT_EQ(root->right->value, 30);
+    ASSERT_EQ(root->right->priority, 120);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
--- test/NodeTest.cc
+++ test/NodeTest.cc
@@ -1,3 +1,23 @@
 //
-// Created by Egor-Always-AFK on 07.12.2023.
+// Created by Egor-Always-AFK on 09.12.2023.
 //
+
+#include <gtest/gtest.h>
+#include "../src/Node.hpp"
+
+TEST(NodeTest, CreateNodeSuccess) {
+    auto node = mit::Node<int>(10);
+
+    ASSERT_EQ(10, node.value);
+    ASSERT_TRUE(node.priority);
+    ASSERT_EQ(nullptr, node.right);
+    ASSERT_EQ(nullptr, node.left);
+    ASSERT_EQ(nullptr, node.parent);
+}
+
+TEST(NodeTest, DifferentNodesHaveDifferentPriorities) {
+    auto firstNode = mit::Node<int>(10);
+    auto secondNode = mit::Node<int>(20);
+
+    ASSERT_NE(firstNode.priority, secondNode.priority);
+}
