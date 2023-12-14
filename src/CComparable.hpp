495c0ec64d0a4ecb29c5e88e0dffc475332936ad
Divium <79859179+Divium-l@users.noreply.github.com>
2023-12-15 01:50:46 +0300

    docs

--- src/CComparable.hpp
+++ src/CComparable.hpp
@@ -4,6 +4,10 @@
 #pragma once
 
 namespace mit {
+    /**
+     * Concept that requires type to have these overloaded operators [<,<=,>,>=,==,!=]
+     * @tparam T type
+     */
     template<class T> concept CComparable = requires(T object, T object2) {
         object <=> object2;
     };
--- src/DecartTree.hpp
+++ src/DecartTree.hpp
@@ -13,13 +13,28 @@
 
 namespace mit {
 
-    template<mit::CComparable T>
-    class DecartTree {
+    /**
+     * In computer science, the treap and the randomized binary search tree
+     * are two closely related forms of binary search tree data structures
+     * that maintain a dynamic set of ordered keys and allow binary searches
+     * among the keys. After any sequence of insertions and deletions of keys,
+     * the shape of the tree is a random variable with the same probability
+     * distribution as a random binary tree; in particular, with high probability
+     * its height is proportional to the logarithm of the number of keys, so that
+     * each search, insertion, or deletion operation takes logarithmic time to perform.
+     */
+    template<mit::CComparable T> class DecartTree {
     private:
         typedef std::shared_ptr<mit::Node<T>> Node;
 
         Node root;
 
+        /**
+         * Recursive method for checking if element exists in the tree
+         * @param value element to search for
+         * @param node tree node to start checking from
+         * @return true/false
+         */
         bool _contains(T value, Node node) {
 			if (node == nullptr) {
 				return false;
@@ -32,6 +47,12 @@ namespace mit {
 			}
         }
 
+        /**
+         * Recursive method for merging two trees into one
+         * @param leftNode root of the first tree
+         * @param rightNode root of the second tree
+         * @return root of the new tree
+         */
         Node _merge(Node leftNode, Node rightNode) {
             if (!leftNode) {
                 return rightNode;
@@ -48,6 +69,12 @@ namespace mit {
             }
         }
 
+        /**
+         * Recursive method for splitting one tree by priority key into two
+         * @param node node to split from
+         * @param keyForSplit priority key
+         * @return pair of roots
+         */
         std::pair<Node, Node> _split(Node node, T keyForSplit) {
             if (!node) {
                 return {nullptr, nullptr};
@@ -64,6 +91,11 @@ namespace mit {
             }
         }
 
+        /**
+         * Recursive method that prints nodes into ostream
+         * @param node node
+         * @param ostream stream
+         */
         static void _inOrderPrint(Node node, std::ostream& ostream) {
             if (node == nullptr) {
                 return;
@@ -80,16 +112,29 @@ namespace mit {
         DecartTree() : root(nullptr) {}
         ~DecartTree() = default;
 
+        /**
+         * Merges this tree with another tree
+         * @param treeForMerge another tree
+         */
         [[maybe_unused]] void merge(mit::DecartTree<T> treeForMerge) {
             this->root = _merge(this->root, treeForMerge.root);
         }
 
+        /**
+         * Splits tree on two subtrees
+         * @param keyForSplit key for split
+         * @return pair of subtrees
+         */
         [[maybe_unused]] std::pair<Node, Node> split(T keyForSplit) {
             auto [leftTree, rightTree] = _split(this->root, keyForSplit);
             this->root = leftTree;
             return {leftTree, rightTree};
         }
 
+        /**
+         * Adds element to tree with random priority
+         * @param value element
+         */
         [[maybe_unused]] void add(T value) {
             auto [leftTree, rightTree] = _split(this->root, value);
             auto newNode = Node(new mit::Node(value));
@@ -97,26 +142,47 @@ namespace mit {
             this->root = _merge(leftTree, _merge(newNode, rightTree));
         }
 
+        /**
+         * Adds element to tree with set priority
+         * @param value element
+         * @param priority priority
+         */
         [[maybe_unused]] void add(T value, int priority) {
             auto [leftTree, rightTree] = _split(this->root, value);
             Node newNode = Node(new mit::Node(value, priority));
             this->root = _merge(leftTree, _merge(newNode, rightTree));
         }
 
-        [[maybe_unused]] void addAll(T *values) {
+        /**
+         * Adds all elements from the array to the tree with random priority
+         * @param values elements
+         */
+        [[maybe_unused]] void addAll(T values[]) {
             for (auto data: values) {
                 this->add(data);
             }
         }
 
+        /**
+         * Checks if element is present in the tree
+         * @param value element
+         * @return true/false
+         */
         [[maybe_unused]] bool contains(T value) {
             return _contains(value, this->root);
         }
 
+        /**
+         * Removes all elements from the tree
+         */
         [[maybe_unused]] void clear() {
             root = nullptr;
         }
 
+        /**
+         * Removes requested element from the tree
+         * @param value element
+         */
         [[maybe_unused]] void remove(T value) {
 			if (this->contains(value)) {
 				auto [left, right] = this->split(value);
@@ -124,12 +190,12 @@ namespace mit {
 			}
 		}
 
+        /**
+         * Returns root of the tree
+         * @return root
+         */
         [[maybe_unused]] Node getRoot() {
             return root;
-        }
-
-        [[maybe_unused]] void map(void (*lambda)(Node node)) {
-
         }
 
 		friend std::ostream& operator<<(std::ostream& ostream, mit::DecartTree<T>& tree) {
