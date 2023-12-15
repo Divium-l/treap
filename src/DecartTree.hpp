2954e8e4026afb984d4e6b9f440f36fe8b6a73e4
Divium <79859179+Divium-l@users.noreply.github.com>
2023-12-15 10:35:43 +0300

    renamed private functions

--- src/DecartTree.hpp
+++ src/DecartTree.hpp
@@ -35,15 +35,15 @@ namespace mit {
          * @param node tree node to start checking from
          * @return true/false
          */
-        bool _contains(T value, Node node) {
+        bool contains_(T value, Node node) {
 			if (node == nullptr) {
 				return false;
 			} else if (node->value == value) {
 				return true;
 			} else if (node->value >= value) {
-				return _contains(value, node->left);
+				return contains_(value, node->left);
 			} else {
-				return _contains(value, node->right);
+				return contains_(value, node->right);
 			}
         }
 
@@ -53,17 +53,17 @@ namespace mit {
          * @param rightNode root of the second tree
          * @return root of the new tree
          */
-        Node _merge(Node leftNode, Node rightNode) {
+        Node merge_(Node leftNode, Node rightNode) {
             if (!leftNode) {
                 return rightNode;
             } else if (!rightNode) {
                 return leftNode;
             } else {
                 if (leftNode->priority > rightNode->priority) {
-                    leftNode->right = _merge(leftNode->right, rightNode);
+                    leftNode->right = merge_(leftNode->right, rightNode);
                     return leftNode;
                 } else {
-                    rightNode->left = _merge(leftNode, rightNode->left);
+                    rightNode->left = merge_(leftNode, rightNode->left);
                     return rightNode;
                 }
             }
@@ -75,16 +75,16 @@ namespace mit {
          * @param keyForSplit priority key
          * @return pair of roots
          */
-        std::pair<Node, Node> _split(Node node, T keyForSplit) {
+        std::pair<Node, Node> split_(Node node, T keyForSplit) {
             if (!node) {
                 return {nullptr, nullptr};
             } else {
                 if (node->value <= keyForSplit) {
-                    auto [left, right] = _split(node->right, keyForSplit);
+                    auto [left, right] = split_(node->right, keyForSplit);
                     node->right = left;
                     return {node, right};
                 } else {
-                    auto [left, right] = _split(node->left, keyForSplit);
+                    auto [left, right] = split_(node->left, keyForSplit);
                     node->left = right;
                     return {left, node};
                 }
@@ -96,7 +96,7 @@ namespace mit {
          * @param node node
          * @param ostream stream
          */
-        static void _inOrderPrint(Node node, std::ostream& ostream) {
+        static void inOrderPrint_(Node node, std::ostream& ostream) {
             if (node == nullptr) {
                 return;
             }
@@ -104,8 +104,8 @@ namespace mit {
                 ostream << *node << ", ";
             }
 
-            _inOrderPrint(node->left, ostream);
-            _inOrderPrint(node->right, ostream);
+            inOrderPrint_(node->left, ostream);
+            inOrderPrint_(node->right, ostream);
         }
 
     public:
@@ -117,7 +117,7 @@ namespace mit {
          * @param treeForMerge another tree
          */
         [[maybe_unused]] void merge(mit::DecartTree<T> treeForMerge) {
-            this->root = _merge(this->root, treeForMerge.root);
+            this->root = merge_(this->root, treeForMerge.root);
         }
 
         /**
@@ -126,7 +126,7 @@ namespace mit {
          * @return pair of subtrees
          */
         [[maybe_unused]] std::pair<Node, Node> split(T keyForSplit) {
-            auto [leftTree, rightTree] = _split(this->root, keyForSplit);
+            auto [leftTree, rightTree] = split_(this->root, keyForSplit);
             this->root = leftTree;
             return {leftTree, rightTree};
         }
@@ -136,10 +136,10 @@ namespace mit {
          * @param value element
          */
         [[maybe_unused]] void add(T value) {
-            auto [leftTree, rightTree] = _split(this->root, value);
+            auto [leftTree, rightTree] = split_(this->root, value);
             auto newNode = Node(new mit::Node(value));
 
-            this->root = _merge(leftTree, _merge(newNode, rightTree));
+            this->root = merge_(leftTree, merge_(newNode, rightTree));
         }
 
         /**
@@ -148,9 +148,9 @@ namespace mit {
          * @param priority priority
          */
         [[maybe_unused]] void add(T value, int priority) {
-            auto [leftTree, rightTree] = _split(this->root, value);
+            auto [leftTree, rightTree] = split_(this->root, value);
             Node newNode = Node(new mit::Node(value, priority));
-            this->root = _merge(leftTree, _merge(newNode, rightTree));
+            this->root = merge_(leftTree, merge_(newNode, rightTree));
         }
 
         /**
@@ -169,7 +169,7 @@ namespace mit {
          * @return true/false
          */
         [[maybe_unused]] bool contains(T value) {
-            return _contains(value, this->root);
+            return contains_(value, this->root);
         }
 
         /**
@@ -186,7 +186,7 @@ namespace mit {
         [[maybe_unused]] void remove(T value) {
 			if (this->contains(value)) {
 				auto [left, right] = this->split(value);
-				this->root = _merge(right, _merge(left->left, left->right));
+				this->root = merge_(right, merge_(left->left, left->right));
 			}
 		}
 
@@ -200,7 +200,7 @@ namespace mit {
 
 		friend std::ostream& operator<<(std::ostream& ostream, mit::DecartTree<T>& tree) {
             ostream << "Treap: [";
-            _inOrderPrint(tree.root, ostream);
+            inOrderPrint_(tree.root, ostream);
             ostream << "]";
             return ostream;
 		}
